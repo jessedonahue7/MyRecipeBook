3fd19c8baae233011a3a95cff9d21f232bf86e8a
jessedonahue7 <71794005+jessedonahue7@users.noreply.github.com>
2020-11-10 13:18:35 -0800

    created serializer in web_app and moved search function to it from views.py

---
+++ web_app/serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Custom_User
+from django.db.models import Q
+from .models import Recipe
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class SearchResultsView(ListView):
+    model = Recipe
+    template_name = 'search_results.html'
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        object_list = Recipe.objects.filter(
+            Q(Name__icontains=query) | Q(Ingredients__icontains=query)
+        )
+        return object_list
