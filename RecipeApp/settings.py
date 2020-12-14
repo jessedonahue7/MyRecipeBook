dee11214724d33bdf1482a8b05d1bbed1289f885
jessedonahue7 <71794005+jessedonahue7@users.noreply.github.com>
2020-12-13 20:52:06 -0800

    Update settings.py

--- RecipeApp/settings.py
+++ RecipeApp/settings.py
@@ -18,7 +18,9 @@ import os
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
