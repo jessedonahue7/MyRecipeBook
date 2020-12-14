ab5abc3f698664b66c5c4968ad895b2bfd294282
jessedonahue7 <71794005+jessedonahue7@users.noreply.github.com>
2020-12-13 20:23:48 -0800

    Update settings.py

--- RecipeApp/settings.py
+++ RecipeApp/settings.py
@@ -15,7 +15,16 @@ import datetime
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
